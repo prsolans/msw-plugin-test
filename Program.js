1172c35276434866843e375aa6f9b4f4ece615fd
Paul R. Solans <prsolans@gmail.com>
2015-12-18 14:30:24 -0600

    Coercion typo

--- Program.js
+++ Program.js
@@ -1,38 +1,42 @@
 // The initialize function is required for all apps.
-Office.initialize = function (reason) {
+Office.initialize = function(reason) {
     // Checks for the DOM to load using the jQuery ready function.
-    $(document).ready(function () {
-    // After the DOM is loaded, app-specific code can run.
-    // Add any initialization logic to this function.
+    $(document).ready(function() {
+        // After the DOM is loaded, app-specific code can run.
+        // Add any initialization logic to this function.
     });
 }
-var MyArray = [['Berlin'],['Munich'],['Duisburg']];
+var MyArray = [
+    ['Berlin'],
+    ['Munich'],
+    ['Duisburg']
+];
 
 function writeData() {
-    Office.context.document.setSelectedDataAsync(MyArray, { coercionType: 'matrix' });
+    Office.context.document.setSelectedDataAsync(MyArray, {
+        coercionType: 'matrix'
+    });
 }
 
 function ReadData() {
-    Office.context.document.getSelectedDataAsync("matrix", function (result) {
-        if (result.status === "succeeded"){
+    Office.context.document.getSelectedDataAsync(Office.CoercionType.Text, function(result) {
+        if (result.status === "succeeded") {
             printData(result.value);
-        }
-
-        else{
+        } else {
             printData(result.error.name + ":" + err.message);
         }
     });
 }
 
-      function printData(data) {
+function printData(data) {
     {
         var printOut = "";
 
-        for (var x = 0 ; x < data.length; x++) {
+        for (var x = 0; x < data.length; x++) {
             for (var y = 0; y < data[x].length; y++) {
                 printOut += data[x][y] + ",";
             }
         }
-       document.getElementById("results").innerText = printOut;
+        document.getElementById("results").innerText = printOut;
     }
 }
