0cefda1ab00eb81beb94e731ad74befed17ca865
Paul R. Solans <prsolans@gmail.com>
2015-12-18 14:41:04 -0600

    Added content control logic

--- Program.js
+++ Program.js
@@ -29,3 +29,42 @@ function printData(data) {
         document.getElementById("results").innerText = printOut;
     }
 }
+
+function readContentControl() {
+    // Run a batch operation against the Word object model.
+    Word.run(function(context) {
+
+            // Create a proxy object for the content controls collection that contains a specific tag.
+            var contentControlsWithTag = context.document.contentControls.getByTag('Customer-Address');
+
+            // Queue a command to load the tag property for all of content controls. 
+            context.load(contentControlsWithTag, 'tag');
+
+            // Synchronize the document state by executing the queued commands, 
+            // and return a promise to indicate task completion.
+            return context.sync().then(function() {
+                if (contentControlsWithTag.items.length === 0) {
+                    console.log('No content control found.');
+                } else {
+                    // Queue a command to get the HTML contents of the first content control.
+                    var html = contentControlsWithTag.items[0].getHtml();
+
+                    printData(html);
+                    
+                    // Synchronize the document state by executing the queued commands, 
+                    // and return a promise to indicate task completion.
+                    return context.sync()
+                        .then(function() {
+                            console.log('Content control HTML: ' + html.value);
+                        });
+                }
+            });
+        })
+        .catch(function(error) {
+            console.log('Error: ' + JSON.stringify(error));
+            if (error instanceof OfficeExtension.Error) {
+                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+            }
+        });
+
+}
