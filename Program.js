a5752f412d0b2e839e74d4126da9e02484e18ba3
Paul R. Solans <prsolans@gmail.com>
2016-02-05 13:22:03 -0600

    Update Program.js

--- Program.js
+++ Program.js
@@ -34,6 +34,8 @@ function readContentControl() {
     // Run a batch operation against the Word object model.
     Word.run(function(context) {
 
+            var printOut = "";
+
             // Create a proxy object for the content controls collection that contains a specific tag.
             var contentControlsWithTag = context.document.contentControls.getByTag('Address');
 
@@ -44,11 +46,11 @@ function readContentControl() {
             // and return a promise to indicate task completion.
             return context.sync().then(function() {
                 if (contentControlsWithTag.items.length === 0) {
-                    console.log("There isn't a content control with a tag of Customer-Address in this document.");
-                    document.getElementById("control-results").innerText = "There isn't a content control with a tag of Customer-Address in this document.";
+                    printOut = "There isn't a content control with a tag of Customer-Address in this document.");
+                    document.getElementById("control-results").innerText = printOut;
                 } else {
-                    console.log('The first content control with the tag of Customer-Address has this text: ' + contentControlsWithTag.items[0].text);
-                    document.getElementById("control-results").innerText = "The first content control with the tag of Customer-Address has this text: " + contentControlsWithTag.items[0].text;
+                    printOut = "The first content control with the tag of Customer-Address has this text: " + contentControlsWithTag.items[0].text);
+                    document.getElementById("control-results").innerText = printOut;
                 }
 
             });
